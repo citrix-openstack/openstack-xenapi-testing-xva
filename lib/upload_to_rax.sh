53626ca6ecd47ac6dbe028ba48e3481e6d5f5146
Mate Lakat <mlakat@mlakat.hu>
2014-12-04 22:05:45 +0100

    Use SCP

--- lib/upload_to_rax.sh
+++ lib/upload_to_rax.sh
@@ -6,4 +6,4 @@ hg clone http://hg.uk.xensource.com/openstack/infrastructure.hg/ infra
 cd infra/osci
 
 ./ssh.sh prod_ci "rm -rf xva_images && mkdir xva_images" < /dev/null
-cat ~/*.xva | ./ssh.sh prod_ci/ dd of=xva_images/image.xva
+./scp.sh prod_ci/ ~/*.xva xva_images/image.xva < /dev/null
