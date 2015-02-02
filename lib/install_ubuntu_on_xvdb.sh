aa4249224d45bfb9ab8d38711e25c5972eed54af
Bob Ball <bob.ball@citrix.com>
2015-02-02 13:41:45 +0000

    Replace defunct mirror.anl.gov mirror

--- lib/install_ubuntu_on_xvdb.sh
+++ lib/install_ubuntu_on_xvdb.sh
@@ -31,7 +31,7 @@ function update_debootstrap() {
 }
 
 function set_mirror() {
-    sudo sed -ie "s,mirror.anl.gov/pub/ubuntu,mirror.anl.gov/ubuntu,g" /etc/apt/sources.list
+    sudo sed -ie "s,mirror.anl.gov/pub/ubuntu,mirror.cc.columbia.edu/pub/linux/ubuntu/archive,g" /etc/apt/sources.list
 }
 
 function prepare_disk() {
@@ -92,7 +92,7 @@ function install_base_system() {
     DEBOOTSTRAP_ARGS+=",strace,gdb"
     DEBOOTSTRAP_ARGS+=" trusty"
     DEBOOTSTRAP_ARGS+=" /ubuntu_chroot"
-    DEBOOTSTRAP_ARGS+=" http://mirror.anl.gov/ubuntu/"
+    DEBOOTSTRAP_ARGS+=" http://mirror.cc.columbia.edu/pub/linux/ubuntu/archive/"
 
     CACHE_MD5=`echo $DEBOOTSTRAP_ARGS | md5sum | cut -f 1 -d ' '`
 
